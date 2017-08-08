7d41cc3be2219d63d052a376bee29e1be9e3882f
monkeyWangs <2424880409@qq.com>
2017-08-08 17:48:50 +0800

    添加响应式布局方案

--- src/main.js
+++ src/main.js
@@ -3,8 +3,12 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Row from './plugins/row/index'
+import Col from './plugins/col/index'
 
 Vue.config.productionTip = false
+Vue.use(Row)
+Vue.use(Col)
 
 /* eslint-disable no-new */
 new Vue({
